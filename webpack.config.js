fe0f7ce8424b4811d6c8da5695c5e9d447aeb699
jincod <vadim.abdrashitov@gmail.com>
2017-03-08 22:53:21 +0500

    moved webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 module.exports = false && process.env.NODE_ENV === 'production' ?
-	require('./src/webpack/webpack.config.prod') :
-	require('./src/webpack/webpack.config.dev');
+	require('./src/client/webpack/webpack.config.prod') :
+	require('./src/client/webpack/webpack.config.dev');
